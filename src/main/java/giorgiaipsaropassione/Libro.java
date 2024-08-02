355d02087f6f82a976ec72fdcb9ab01931bb0867
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-08-02 11:31:51 +0200

    commit class libro

--- src/main/java/giorgiaipsaropassione/Libro.java
+++ src/main/java/giorgiaipsaropassione/Libro.java
@@ -1,5 +1,40 @@
 package giorgiaipsaropassione;
 
-public class Libro {
-    
+import java.time.LocalDate;
+
+public class Libro extends Pubblicazione {
+    private String autore;
+    private Genere genere;
+
+    public Libro(long codiceIsbn, String titolo, LocalDate annoPubblicazione, int numPagine, String autore, Genere genere) {
+        super(codiceIsbn, titolo, annoPubblicazione, numPagine);
+        this.setAutore(autore);
+        this.setGenere(genere);
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public Genere getGenere() {
+        return genere;
+    }
+
+    public void setGenere(Genere genere) {
+        this.genere = genere;
+    }
+
+    @Override
+    public String toString() {
+        return "ISBN: " + getCodiceIsbn() +
+                " - Title: " + getTitolo() +
+                " - Anno Pubblicazione: " + getAnnoPubblicazione() +
+                " - Pagine: " + getNumPagine() +
+                " - Autore: " + getAutore() +
+                " - Genere: " + getGenere();
+    }
 }
