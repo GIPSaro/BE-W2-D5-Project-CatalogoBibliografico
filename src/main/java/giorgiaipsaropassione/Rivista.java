93fb9cd58f3f4e2edcf922d17fbc1c704c210767
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-08-02 11:01:21 +0200

    commit class rivista

--- src/main/java/giorgiaipsaropassione/Rivista.java
+++ src/main/java/giorgiaipsaropassione/Rivista.java
@@ -1,4 +1,29 @@
 package giorgiaipsaropassione;
 
-public class Rivista {
+import java.time.LocalDate;
+
+public class Rivista extends Pubblicazione {
+    private Periodicita periodicita;
+
+    public Rivista(long codiceIsbn, String titolo, LocalDate annoPubblicazione, int numPagine, Periodicita periodicita) {
+        super(codiceIsbn, titolo, annoPubblicazione, numPagine);
+        this.setPeriodicita(periodicita);
+    }
+
+    public Periodicita getPeriodicita() {
+        return periodicita;
+    }
+
+    public void setPeriodicita(Periodicita periodicita) {
+        this.periodicita = periodicita;
+    }
+
+    @Override
+    public String toString() {
+        return "ISBN: " + getCodiceIsbn() +
+                " - Title: " + getTitolo() +
+                " - Anno Pubblicazione: " + getAnnoPubblicazione() +
+                " - Pagine: " + getNumPagine() +
+                " - Periodico: " + getPeriodicita();
+    }
 }
