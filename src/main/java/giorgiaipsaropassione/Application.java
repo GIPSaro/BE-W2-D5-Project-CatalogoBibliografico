b60242f588672e2422818757befb29adc528e25e
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-08-02 14:36:59 +0200

    commit ricerca per data di pubblicazione

--- src/main/java/giorgiaipsaropassione/Application.java
+++ src/main/java/giorgiaipsaropassione/Application.java
@@ -3,6 +3,8 @@ package giorgiaipsaropassione;
 import com.github.javafaker.Faker;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
@@ -66,6 +68,9 @@ public class Application {
 
         // CHIAMATA PER CARCARE LIBRO O RIVISTA TRAMITE SCANNER E RICHIESTA INSERIMENDO ISBN
         cercaPubblicazione(catalogo);
+
+        //CHIAMATA PER CERCARE LIBRO O RIVISTRA TRAMITE ANNO DI PUBBLICAZIONE
+        cercaPubblicazionePerAnno(catalogo);
     }
     //QUI APPLICO UN METODO RANDOM PER RANDOMIZZARE LA CREAZIONE DI LIBRI E RIVISTE (25 e 25)
 
@@ -122,7 +127,7 @@ public class Application {
 
         Pubblicazione libroDaRimuovere = null;
         for (Pubblicazione pubblicazione : catalogo) {
-            if (pubblicazione instanceof Libro && ((Libro) pubblicazione).getCodiceIsbn() == isbnDaEliminare) {
+            if (pubblicazione instanceof Libro && pubblicazione.getCodiceIsbn() == isbnDaEliminare) {
                 libroDaRimuovere = pubblicazione;
                 break;
             }
@@ -144,11 +149,11 @@ public class Application {
         long isbnDaCercare = scanner.nextLong();
 
         for (Pubblicazione pubblicazione : catalogo) {
-            if (pubblicazione instanceof Libro && ((Libro) pubblicazione).getCodiceIsbn() == isbnDaCercare) {
-                System.out.println("Libro trovato: " + pubblicazione.toString());
+            if (pubblicazione instanceof Libro && pubblicazione.getCodiceIsbn() == isbnDaCercare) {
+                System.out.println("Libro trovato: " + pubblicazione);
                 return;
-            } else if (pubblicazione instanceof Rivista && ((Rivista) pubblicazione).getCodiceIsbn() == isbnDaCercare) {
-                System.out.println("Rivista trovata: " + pubblicazione.toString());
+            } else if (pubblicazione instanceof Rivista && pubblicazione.getCodiceIsbn() == isbnDaCercare) {
+                System.out.println("Rivista trovata: " + pubblicazione);
                 return;
             }
         }
@@ -156,4 +161,36 @@ public class Application {
         System.out.println("Nessun libro o rivista trovato con ISBN " + isbnDaCercare);
     }
 
+    //METODO PER CARCARE UN LIBRO UNA RIVISTRA TRAMITE ANNO DI PUBBLICAZIONE
+
+    private static void cercaPubblicazionePerAnno(Set<Pubblicazione> catalogo) {
+        Scanner scanner = new Scanner(System.in);
+        LocalDate dataDaCercare = null;
+        boolean inputValido = false;
+
+        while (!inputValido) {
+            System.out.print("Inserisci l'anno di pubblicazione del libro o rivista da cercare: ");
+            String input = scanner.next();
+            try {
+                //ho dovuto usare LocalDate.parse e DateTimeFormatter.ISO_LOCAL_DATE per permettere l'inserimento
+                // della data nel formato yyyy-mm-dd
+                dataDaCercare = LocalDate.parse(input, DateTimeFormatter.ISO_LOCAL_DATE);
+                inputValido = true;
+            } catch (DateTimeParseException e) {
+                System.out.println("Input non valido. Per favore, inserisci una data nel formato yyyy-MM-dd.");
+            }
+        }
+        int annoDaCercare = dataDaCercare.getYear();
+        boolean trovata = false;
+        for (Pubblicazione pubblicazione : catalogo) {
+            if (pubblicazione.getAnnoPubblicazione().getYear() == annoDaCercare) {
+                System.out.println("Pubblicazione trovata: " + pubblicazione);
+                trovata = true;
+            }
+        }
+
+        if (!trovata) {
+            System.out.println("Nessuna pubblicazione trovata per l'anno " + annoDaCercare);
+        }
+    }
 }
